d94c6d26bc3914c23d4d25215f9df2ea45b9b66f
Thomas Stensitzki <thomas@stensitzki.de>
2017-03-23 19:03:30 +0100

    Comments added, variable consolidation

--- ResizeImage/Program.cs
+++ ResizeImage/Program.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿// MIT License
+// 
+// Copyright (c) 2017 Granikos GmbH & Co. KG (https://www.granikos.eu)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -14,23 +36,29 @@ namespace Granikos.ResizeImage
     {
         private readonly static Regex RegexExtension;
 
-        private static ConsoleColor _foreColor;
+        private static ConsoleColor _foregroundColor;
 
         static Program()
         {
             Program.RegexExtension = new Regex("^\\.?(.+)", RegexOptions.Compiled);
         }
 
-        public Program()
-        {
-        }
-
+        /// <summary>
+        /// Get image codec for given MIME Type
+        /// </summary>
+        /// <param name="mimeType">String containing the MIME type</param>
+        /// <returns>ImageCodecInfo</returns>
         private static ImageCodecInfo GetEncoderInfo(string mimeType)
         {
             ImageCodecInfo.GetImageEncoders();
             return ImageCodecInfo.GetImageEncoders().FirstOrDefault<ImageCodecInfo>((ImageCodecInfo t) => t.MimeType == mimeType);
         }
 
+        /// <summary>
+        /// Resize an image depending on settings file and save it to the outputDir
+        /// </summary>
+        /// <param name="file">Image file with absolute file path</param>
+        /// <param name="outputDir">Target directory for saving the resized image</param>
         private static void ResizeImage(string file, string outputDir)
         {
             try
@@ -89,35 +117,51 @@ namespace Granikos.ResizeImage
             }
         }
 
+        /// <summary>
+        /// Main program
+        /// </summary>
+        /// <param name="args">String array containing the arguments
+        /// ARG1 = Absolute path to source directory
+        /// ARG2 = Absolute path to target directory
+        /// ARG3 .. ARGx = FIle extionsion (jpg, bmp, etc.)
+        /// </param>
         private static void Main(string[] args)
         {
             IEnumerable<string> argStrings;
-            Program._foreColor = Console.ForegroundColor;
-            string str = args.FirstOrDefault<string>();
-            string str1 = args.Skip<string>(1).FirstOrDefault<string>();
-            if (string.IsNullOrWhiteSpace(str))
+            Program._foregroundColor = Console.ForegroundColor;
+            string argStr = args.FirstOrDefault<string>();
+            string argStr1 = args.Skip<string>(1).FirstOrDefault<string>();
+
+            if (string.IsNullOrWhiteSpace(argStr))
             {
                 Program.Usage();
                 return;
             }
-            if (str.Contains("?"))
+
+            if (argStr.Contains("?"))
             {
                 Program.Usage();
                 return;
             }
-            if (!Directory.Exists(str))
+
+            if (!Directory.Exists(argStr))
             {
+                // If source directory does not exist -> exit
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(string.Concat("Could not find directory ", str));
+                Console.WriteLine(string.Concat("Could not find directory ", argStr));
                 Program.Usage();
                 Environment.Exit(1);
             }
-            if (!Directory.Exists(str1))
+
+            if (!Directory.Exists(argStr1))
             {
-                Directory.CreateDirectory(str1);
+                // Create target directory, if not exists
+                Directory.CreateDirectory(argStr1);
             }
+
             if (args.Count<string>() > 2)
             {
+                // Handle file extensions
                 argStrings =
                     from s in args.Skip<string>(2).Distinct<string>(StringComparer.InvariantCultureIgnoreCase)
                     from s2 in s.Split(new char[] { ',', ' ', ';' }, StringSplitOptions.RemoveEmptyEntries)
@@ -135,22 +179,27 @@ namespace Granikos.ResizeImage
                     where m.Success
                     select string.Concat(".", m.Value);
             }
-            IEnumerable<string> strs1 = argStrings;
+
+            IEnumerable<string> extensionStrings = argStrings;
             foreach (string str2 in (
-                from f in Directory.GetFiles(str)
-                from ext in strs1
-                where f.EndsWith(ext)
-                select f).Distinct<string>())
+                from files in Directory.GetFiles(argStr)
+                from extension in extensionStrings
+                where files.EndsWith(extension)
+                select files).Distinct<string>())
             {
-                Program.ResizeImage(str2, str1);
+                Program.ResizeImage(str2, argStr1);
             }
         }
 
+        /// <summary>
+        /// Usage information written to console
+        /// </summary>
         private static void Usage()
         {
-            Console.ForegroundColor = Program._foreColor;
-            Console.WriteLine("Resize Image");
-            Console.WriteLine("(c) 2017 Granikos GmbH 6 Co. KG");
+            // Write some fancy usage information
+            Console.ForegroundColor = Program._foregroundColor;
+            Console.WriteLine("Resize Image Tool");
+            Console.WriteLine("(c) 2017 Granikos GmbH & Co. KG");
             Console.WriteLine();
             Console.WriteLine("ResizeImage.exe SOURCE TARGET [EXT] [EXT] [EXT] ...");
             Console.WriteLine();
